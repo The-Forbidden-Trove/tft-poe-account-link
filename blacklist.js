1ee5b398718d5e9205f99addb4c7b911536b2482
Jzir <>
2021-07-29 16:48:11 +0100

    deal with quotes so we can parse all lines

--- blacklist.js
+++ blacklist.js
@@ -4,10 +4,14 @@ const parse = require('csv-parse/lib/sync')
 const getBlacklistedAccountNames = async () => {
     const blacklistRes = await nodeFetch('https://raw.githubusercontent.com/The-Forbidden-Trove/ForbiddenTroveBlacklist/main/blacklist.csv');
     const csvText = await blacklistRes.text()
-    return parse(csvText.replace(/['"]+/g, ''), {
-        skipLinesWithError: true,
+    return parse(csvText, {
         columns: true,
-        skip_empty_lines: true
+        quote: '"',
+        delimiter: ',',
+        rtrim: true,
+        ltrim: true,
+        skip_empty_lines: true,
+        
     }).map((csvEntry) => csvEntry.account_name.toLowerCase());
 }
 
