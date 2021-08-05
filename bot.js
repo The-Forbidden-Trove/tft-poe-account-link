a806da1c1615c11804561f84005e7bb2e82b72bc
Jzir <75438007+Jzir@users.noreply.github.com>
2021-08-05 17:22:25 +0100

    maybe this?

--- bot.js
+++ bot.js
@@ -20,6 +20,9 @@ if (process.env.NODE_ENV === 'dev' && process.env.testEnvProp === undefined) {
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity('Message \'link\' to verify')
+    .then(presence => console.log(`Activity set to ${JSON.stringify(presence)}`))
+    .catch((err) => console.log(`ERROR SETTING ACTIVITY: ${err}`));
 });
 
 client.on('message', async (message) => {
