b0f022a951c1bff7052aa79b0e8df3a4894f5b83
gluitina <gluitina.tft@gmail.com>
2023-04-26 19:39:44 +0200

    update bot.js

--- accountLinkServer.js
+++ accountLinkServer.js
@@ -50,7 +50,7 @@ app.get('/oauth_redirect', async (req, res) => {
       grant_type: "authorization_code",
       code: req.query.code,
       redirect_uri: "https://theforbiddentrove.xyz/oauth_redirect",
-      scope: "account:profile account:characters",
+      scope: "account:profile",
       state: req.query.state
     }),
     headers: {
