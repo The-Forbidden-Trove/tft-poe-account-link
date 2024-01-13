87abc9905c00a5f609b8a9e10b5b6c056309d084
Jzir <>
2024-01-13 14:57:51 +0000

    bot stuff

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({
+  partials: ['CHANNEL'],
   presence: {
     activityName: 'DM me to verify',
     activityType: 'PLAYING'
